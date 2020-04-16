61c41ea8d2ca03304236bf08d57f2a969579beec
farhadaniaz <farhadaniaz@gmail.com>
2020-04-15 21:40:38 -0400

    I made the changes

--- src/main/java/com/example/SpringMySQL/api/OrdersController.java
+++ src/main/java/com/example/SpringMySQL/api/OrdersController.java
@@ -2,8 +2,6 @@ package com.example.SpringMySQL.api;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,17 +15,49 @@ import com.example.SpringMySQL.model.Order;
 @RequestMapping("/order")
 public class OrdersController {
     @Autowired
-    private OrdersRepository dao;
+    private OrdersRepository dataccess;
+    private Order someorder = new Order();
 
-    @PostMapping("/bookOrders") //ur putting stuff in the table
-    public String bookOrder(@RequestBody List<Order> orders) {
-        dao.save(orders);
-        return "order inserted : " + orders.size();
+    @PostMapping("/bookOrders") //ur putting stuff in the table, List<Order> orders
+    public String bookOrder(@RequestBody Order someorder) {
+        someorder.setId(1);
+        someorder.setId_raw(1);
+        someorder.setOrder_id("1");
+        someorder.setCustomer("Nat");
+        someorder.setType("Null");
+        someorder.setShip_to("His home");
+        someorder.setShip_to_address("344 Kings road");
+        someorder.setShip_by_date("2020-11-02");
+        someorder.setPack_by_date("2020-08-01");
+        someorder.setInhouse_date("2020-12-05");
+        someorder.setPo_date("2020-12-05");
+        someorder.setJob_date("2020-12-05");
+        someorder.setSource_facility("Home Depot");
+        someorder.setShip_via("UPS");
+        someorder.setPart_number("32");
+        someorder.setPart_quantity(6);
+        someorder.setPart_description("Office Chair");
+        someorder.setEscalate("Null");
+        someorder.setJob_required("Null");
+        someorder.setLine2("Null");
+        someorder.setResponse_accept("Null");
+        someorder.setEpicor_order_id(6);
+        someorder.setJob_number("18");
+        someorder.setDate_created("2020-03-01 22:30:23");
+        someorder.setDate_updated("2020-03-01 22:30:23");
+        someorder.setDate_picked_up("2020-03-02 01:15:03");
+        someorder.setDate_completed("2020-03-01 22:30:23");
+        someorder.setTime_taken("03:22:40");
+        someorder.setRetrieve_count(6);
+        someorder.setAudit_status("In Progress");
+
+        dataccess.save(someorder);
+        return "order booked for  " + someorder.getCustomer();
     }
 
     @GetMapping("/getTickets")  //U wanna get the whole list/table
     public List<Order> getTickets() {
-        return (List<Order>) dao.findAll();
+        return (List<Order>) dataccess.findAll();
     }
 }
 
--- src/main/java/com/example/SpringMySQL/model/Order.java
+++ src/main/java/com/example/SpringMySQL/model/Order.java
@@ -22,7 +22,7 @@ import lombok.ToString;
 public class Order {
     @Id
     @GeneratedValue
-    private int id;
+    private Integer id;
     private int id_raw;
     private String order_id;
     private String customer;
@@ -58,4 +58,65 @@ public class Order {
     String time_taken = simpleDateFormat2.format(new Date());
     private int retrieve_count;
     private String audit_status;
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public Integer getId_raw() { return id_raw; }
+    public void setId_raw(Integer id_raw) { this.id_raw = id_raw; }
+    public String getOrder_id() { return order_id; }
+    public void setOrder_id(String order_id) { this.order_id = order_id; }
+    public String getCustomer() { return customer; }
+    public void setCustomer(String customer) { this.customer = customer; }
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+    public String getShip_to() { return ship_to; }
+    public void setShip_to(String ship_to) { this.ship_to = ship_to; }
+    public String getShip_to_address() { return ship_to_address; }
+    public void setShip_to_address(String ship_to_address) { this.ship_to_address = ship_to_address; }
+    public String getShip_by_date() { return ship_by_date; }
+    public void setShip_by_date(String ship_by_date) { this.ship_by_date = ship_by_date; }
+    public String getPack_by_date() { return pack_by_date; }
+    public void setPack_by_date(String pack_by_date) { this.pack_by_date = pack_by_date; }
+    public String getInhouse_date() { return inhouse_date; }
+    public void setInhouse_date(String inhouse_date) { this.inhouse_date = inhouse_date; }
+    public String getPo_date() { return po_date; }
+    public void setPo_date(String po_date) { this.po_date = po_date; }
+    public String getJob_date() { return job_date; }
+    public void setJob_date(String job_date) { this.job_date = job_date; }
+    public String getSource_facility() { return source_facility; }
+    public void setSource_facility(String source_facility) { this.source_facility = source_facility; }
+    public String getShip_via() { return ship_via; }
+    public void setShip_via(String ship_via) { this.ship_via = ship_via; }
+    public String getPart_number() { return part_number; }
+    public void setPart_number(String part_number) { this.part_number = part_number; }
+    public int getPart_quantity() { return part_quantity; }
+    public void setPart_quantity(int part_quantity) { this.part_quantity = part_quantity; }
+    public String getPart_description() { return part_description; }
+    public void setPart_description(String part_description) { this.part_description = part_description; }
+    public String getEscalate() { return escalate; }
+    public void setEscalate(String escalate) { this.escalate = escalate; }
+    public String getJob_required() { return job_required; }
+    public void setJob_required(String job_required) { this.job_required = job_required; }
+    public String getLine2() { return line2; }
+    public void setLine2(String line2) { this.line2 = line2; }
+    public String getResponse_accept() { return response_accept; }
+    public void setResponse_accept(String response_accept) { this.response_accept = response_accept; }
+    public int getEpicor_order_id() { return epicor_order_id; }
+    public void setEpicor_order_id(int epicor_order_id) { this.epicor_order_id = epicor_order_id; }
+    public String getJob_number() { return job_number; }
+    public void setJob_number(String job_number) { this.job_number = job_number; }
+    public String getDate_created() { return date_created; }
+    public void setDate_created(String date_created) { this.date_created = date_created; }
+    public String getDate_updated() { return date_updated; }
+    public void setDate_updated(String date_updated) { this.date_updated = date_updated; }
+    public String getDate_picked_up() { return date_picked_up; }
+    public void setDate_picked_up(String date_picked_up) { this.date_picked_up = date_picked_up; }
+    public String getDate_completed() { return date_completed; }
+    public void setDate_completed(String date_completed) { this.date_completed = date_completed; }
+    public String getTime_taken() { return time_taken; }
+    public void setTime_taken(String time_taken) { this.time_taken = time_taken; }
+    public int getRetrieve_count() { return retrieve_count; }
+    public void setRetrieve_count(int retrieve_count) { this.retrieve_count = retrieve_count; }
+    public String getAudit_status() { return audit_status; }
+    public void setAudit_status(String audit_status) { this.audit_status = audit_status; }
 }
